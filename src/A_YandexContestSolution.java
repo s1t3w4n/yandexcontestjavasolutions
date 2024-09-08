1f26d3e3f86fddc65ded3fbc2f06846ccd9d2b5b
s1t3w4n <s1t3w4n@yandex.ru>
2024-09-09 00:03:42 +0300

    A contest Solution

---
+++ src/A_YandexContestSolution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class A_YandexContestSolution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        String J = r.readLine();
+        String S = r.readLine();
+
+        int result = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            char ch = S.charAt(i);
+            if (J.indexOf(ch) >= 0) {
+                ++result;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
